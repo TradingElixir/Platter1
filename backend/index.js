8ba3b6321a4a08998915f8ec5d40ec302f97f752
utkarsh <utkarsh@esamudaay.com>
2023-01-10 00:41:17 +0530

    fixed nft not getting fetched issue

--- backend/index.js
+++ backend/index.js
@@ -12,35 +12,31 @@ app.get("/nativeBalance", async (req, res) => {
     const { address, chain } = req.query;
     let balanc;
 
-      balanc = await Moralis.EvmApi.balance.getNativeBalance({
-        address: address,
-        chain: chain,
-      });
-    
+    balanc = await Moralis.EvmApi.balance.getNativeBalance({
+      address: address,
+      chain: chain,
+    });
+
     const result2 = balanc.raw;
 
-    let wallet
-    if (chain === '0xfa'){
-    wallet = '0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83';
-    }else if (chain === '0x1'){
-      wallet = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2';
-    }else if (chain === '0x89'){
-      wallet = '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270';
-    }else if (chain === '0x38'){
-      wallet = '0x242a1ff6ee06f2131b7924cacb74c7f9e3a5edc9';
+    let wallet;
+    if (chain === "0xfa") {
+      wallet = "0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83";
+    } else if (chain === "0x1") {
+      wallet = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+    } else if (chain === "0x89") {
+      wallet = "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270";
+    } else if (chain === "0x38") {
+      wallet = "0x242a1ff6ee06f2131b7924cacb74c7f9e3a5edc9";
     }
 
-
     const response = await Moralis.EvmApi.token.getTokenPrice({
-      address:wallet,
+      address: wallet,
       chain,
     });
     const result1 = response.raw;
 
-    
-
     return res.status(200).json({ result2, result1 });
-    
   } catch (e) {
     console.log(e);
     console.log("something went wrong");
@@ -48,12 +44,11 @@ app.get("/nativeBalance", async (req, res) => {
   }
 });
 
-
 app.get("/tokenBalances", async (req, res) => {
   try {
     const { address, chain } = req.query;
-    
-   const response = await Moralis.EvmApi.token.getWalletTokenBalances({
+
+    const response = await Moralis.EvmApi.token.getWalletTokenBalances({
       address: address,
       chain: chain,
     });
@@ -67,26 +62,29 @@ app.get("/tokenBalances", async (req, res) => {
           chain: chain,
         });
         const price = priceResponse.raw;
-  
+
         if (price.usdPrice > 0.01) {
           tokens[i].usd = price.usdPrice;
           legitTokens.push(tokens[i]);
         } else {
           console.log("💩 coin");
         }
-     } catch (e) {
+      } catch (e) {
         console.log(e);
       }
-      const t=legitTokens;
-    for (let i = 0; i < t.length; i++) {
-      t[i].bal = (Number(t[i].balance) / Number(`1E${t[i].decimals}`)).toFixed(3); //1E18
-      t[i].val = ((Number(t[i].balance) / Number(`1E${t[i].decimals}`)) *Number(t[i].usd)).toFixed(2);
-    }
+      const t = legitTokens;
+      for (let i = 0; i < t.length; i++) {
+        t[i].bal = (
+          Number(t[i].balance) / Number(`1E${t[i].decimals}`)
+        ).toFixed(3); //1E18
+        t[i].val = (
+          (Number(t[i].balance) / Number(`1E${t[i].decimals}`)) *
+          Number(t[i].usd)
+        ).toFixed(2);
+      }
     }
 
     return res.status(200).json({ legitTokens });
-    
-    
   } catch (e) {
     console.log(e);
     console.log("something went wrong");
@@ -95,8 +93,6 @@ app.get("/tokenBalances", async (req, res) => {
 });
 
 app.get("/tokenTransfers", async (req, res) => {
-  
-
   try {
     const { address, chain } = req.query;
 
@@ -104,34 +100,29 @@ app.get("/tokenTransfers", async (req, res) => {
       address: address,
       chain: chain,
     });
-    
+
     const userTrans1 = response.raw;
     const userTrans = userTrans1.result;
 
     let userTransDetails = [];
-    
+
     for (let i = 0; i < userTrans.length; i++) {
-      
       try {
         const metaResponse1 = await Moralis.EvmApi.token.getTokenMetadata({
           addresses: [userTrans[i].address],
           chain: chain,
         });
-           
-          const metaResponse=metaResponse1.raw;
 
-          userTrans[i].decimals = metaResponse.decimals;
-          userTrans[i].symbol = metaResponse.symbol;
-          userTransDetails.push(userTrans[i]);
-        
+        const metaResponse = metaResponse1.raw;
+
+        userTrans[i].decimals = metaResponse.decimals;
+        userTrans[i].symbol = metaResponse.symbol;
+        userTransDetails.push(userTrans[i]);
       } catch (e) {
         console.log(e);
       }
-
     }
     return res.status(200).json({ userTransDetails });
-    
-    
   } catch (e) {
     console.log(e);
     console.log("something went wrong");
@@ -140,20 +131,15 @@ app.get("/tokenTransfers", async (req, res) => {
 });
 
 app.get("/nftBalance", async (req, res) => {
- 
-
   try {
     const { address, chain } = req.query;
-
-    const response1 = await Moralis.EvmApi.nft.getWalletNFTs({
-      address: address,
-      chain: chain,
+    console.log(`nft balance function chain => ${chain} address => ${address}`);
+    const response = await Moralis.EvmApi.nft.getWalletNFTs({
+      address,
+      chain,
     });
-
-    const userNFTs = response1.raw;
-  
+    const userNFTs = response?.result;
     return res.status(200).json({ userNFTs });
-    
   } catch (e) {
     console.log(e);
     console.log("something went wrong");
@@ -161,14 +147,10 @@ app.get("/nftBalance", async (req, res) => {
   }
 });
 
-
-
 Moralis.start({
   apiKey: "RGu7JApF36seSKwH6bClRjYlQyuyRtzbvfrBIrb3wFSBUqdrPb4AXfrG7y3vC68x",
 }).then(() => {
   app.listen(port, () => {
     console.log(`Listening for API Calls`);
-    
-    
   });
 });
--- frontend/src/components/Nfts.js
+++ frontend/src/components/Nfts.js
@@ -1,58 +1,56 @@
-import React from 'react'
+import React from "react";
 import axios from "axios";
 
-
-function Nfts({chain, wallet, nfts, setNfts}) {
-
-
+function Nfts({ chain, wallet, nfts, setNfts }) {
   async function getUserNfts() {
-     
-     const response = await axios.get("http://localhost:3000/nftBalance", {
+    const response = await axios.get("http://localhost:3000/nftBalance", {
       params: {
         address: wallet,
         chain: chain,
       },
     });
-      
-       if (response.data.result) {
-        nftProcessing(response.data.result);
-      }
+
+    if (response.data) {
+      // nftProcessing(response.data);
+      setNfts(response.data.userNFTs);
     }
-  
-    function nftProcessing(t) {
-      for (let i = 0; i < t.length; i++) {
-        let meta = JSON.parse(t[i].metadata);
-        if (meta && meta.image) {
-          if (meta.image.includes(".")) {
-            t[i].image = meta.image;
-          } else {
-            t[i].image = "https://ipfs.moralis.io:2053/ipfs/" + meta.image;
-          }
+  }
+
+  function nftProcessing(t) {
+    for (let i = 0; i < t.length; i++) {
+      let meta = t[i].metadata;
+      if (meta && meta.image) {
+        if (meta.image.includes(".")) {
+          t[i].image = meta.image;
+        } else {
+          t[i].image = "https://ipfs.moralis.io:2053/ipfs/" + meta.image;
         }
       }
-      setNfts(t);
-      
     }
+    setNfts(t);
+  }
 
   return (
-    <><h1>Portfolio NFTs</h1>
-    <div>
-      <button onClick={getUserNfts}>Fetch NFTs</button>
-      
-      <br />
-      {nfts &&
-        nfts.map((e) => {
-          return (
-            <>
-              {e.image && <img src={e.image} width={200} />}
-              <span>Name: {e.name}, </span>
-              <span>(ID: {e.token_id})</span>
-              <br />
-            </>
-          );
-        })}
-    </div></>
-  )
-      }
+    <>
+      <h1>Portfolio NFTs</h1>
+      <div>
+        <button onClick={getUserNfts}>Fetch NFTs</button>
+
+        <br />
+        {nfts &&
+          nfts.map((e) => {
+            return (
+              <>
+                {e.metadata.image && <img src={e.metadata.image} width={200} />}
+                <span>Name: {e.metadata.name}, </span>
+                <span>(ID: {e.metadata.token_id})</span>
+                <br />
+              </>
+            );
+          })}
+      </div>
+    </>
+  );
+}
 
 export default Nfts;
